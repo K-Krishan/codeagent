85b0b8ef778d7e1275ee8b211763c2ac868fadb9
K-Krishan <krishna7065178487@gmail.com>
2025-11-11 03:50:08 +0530

    impl. running codes as subprocess

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import subprocess
 import os
 
 def run_python_file(current_directory, filepath, args=[]):
@@ -12,3 +13,13 @@ def run_python_file(current_directory, filepath, args=[]):
         return f'Error: File doesn\'t exist: "{filepath}"'
     if not dest.is_file() or dest.suffix != '.py':
         return f'Error: "{filepath}" is not a python file'
+    try:
+        output = subprocess.run(["python", dest, *args], cwd=pwd, timeout=30, capture_output=True)
+        returnstring = f"""
+            STDOUT: {output.stdout},
+            STDERR: {output.stderr},
+            Process returned with return code {output.returncode}
+    """
+        return returnstring
+    except Exception as e:
+        return f'Error: Error executing {filepath}: {e}'
--- test.py
+++ test.py
@@ -1,30 +1,39 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def test_files_info():
-    print(get_files_info("Calc", "."))
+    print(get_files_info("calc", "."))
     print()
-    print(get_files_info("Calc", "pkg"))
+    print(get_files_info("calc", "pkg"))
     print()
-    print(get_files_info("Calc", "/bin"))
+    print(get_files_info("calc", "/bin"))
     print()
-    print(get_files_info("Calc", "../"))
+    print(get_files_info("calc", "../"))
     print()
 
 def test_file_content():
-    print(get_file_content("Calc", "main.py"))
+    print(get_file_content("calc", "main.py"))
     print()
-    print(get_file_content("Calc", "pkg/calculator.py"))
+    print(get_file_content("calc", "pkg/calculator.py"))
     print()
-    print(get_file_content("Calc", "/bin/cat"))
+    print(get_file_content("calc", "/bin/cat"))
     print()
-    print(get_file_content("Calc", "pkg/does_not_exist.py"))
+    print(get_file_content("calc", "pkg/does_not_exist.py"))
     print()
 def test_file_write():
-    print(write_file("Calc", "lorem.txt", "wait, this isn't lorem ipsum"))
-    print(write_file("Calc", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-    print(write_file("Calc", "/tmp/temp.txt", "this should not be allowed"))
+    print(write_file("calc", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calc", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calc", "/tmp/temp.txt", "this should not be allowed"))
+def test_run_python():
+    print(run_python_file("calc", "main.py"))
+    print(run_python_file("calc", "main.py", ["3 + 5"]))
+    print(run_python_file("calc", "tests.py"))
+    print(run_python_file("calc", "../main.py"))
+    print(run_python_file("calc", "nonexistent.py"))
+    print(run_python_file("calc", "lorem.txt"))
 # test_files_info()
 # test_file_content()
-test_file_write()
+# test_file_write()
+test_run_python()
